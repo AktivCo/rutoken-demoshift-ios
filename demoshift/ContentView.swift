4f882409a18d6f2100f3744e5a490ecd4b3b820a
Andrey Trifonov <trifonov@rutoken.ru>
2020-05-13 14:28:42 +0300

    Initial Commit

---
+++ demoshift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  demoshift
+//
+//  Created by Андрей Трифонов on 13.05.2020.
+//  Copyright © 2020 Aktiv Co. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
