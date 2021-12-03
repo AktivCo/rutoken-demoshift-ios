b0461ca73e9a1c0789464a571d5ad277a17db8d6
Александр Иванов <>
2021-12-03 14:43:51 +0300

    Add button for navigate to AddVcrView
    Resolve: #10

---
+++ demoshift/Views/AddVcrView.swift
@@ -0,0 +1,16 @@
+//
+//  AddVcrView.swift
+//  demoshift
+//
+//  Created by Александр Иванов on 02.12.2021.
+//  Copyright © 2021 Aktiv Co. All rights reserved.
+//
+
+import SwiftUI
+
+
+struct AddVcrView: View {
+    var body: some View {
+        Text("QR-code")
+    }
+}
--- demoshift/Views/VcrListView.swift
+++ demoshift/Views/VcrListView.swift
@@ -10,13 +10,46 @@ import SwiftUI
 
 
 struct VcrListView: View {
+    @State var showAddVcrView = false
+
     var body: some View {
-        List {
-            VCRCard(name: "iPhone (Sasha) - VCR", isActive: false)
-            VCRCard(name: "iPhone (Masha) - VCR", isActive: true)
-            VCRCard(name: "iPhone (Ragnaros) - VCR", isActive: false)
-            VCRCard(name: "iPhone (Stas) - VCR", isActive: true)
-            VCRCard(name: "iPhone (SAS) - VCR", isActive: false)
+        NavigationLink(destination: AddVcrView(),
+                       isActive: self.$showAddVcrView) {
+            EmptyView()
         }
+        .isDetailLink(false)
+
+        ZStack {
+            VStack {
+                Text("Доступные считыватели")
+                    .font(.headline)
+                    .padding(.top)
+                List {
+                    VCRCard(name: "iPhone (Sasha) - VCR", isActive: false)
+                    VCRCard(name: "iPhone (Masha) - VCR", isActive: true)
+                    VCRCard(name: "iPhone (Ragnaros) - VCR", isActive: false)
+                    VCRCard(name: "iPhone (Stas) - VCR", isActive: true)
+                    VCRCard(name: "iPhone (SAS) - VCR", isActive: false)
+                }
+            }
+            VStack(alignment: .trailing) {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        showAddVcrView = true
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .imageScale(.large)
+                            .font(.system(size: 80))
+                            .foregroundColor(Color("button-background"))
+                            .background(Color("view-background").mask(Circle()).scaleEffect(0.8))
+                    }
+                }
+                .padding(.bottom, 20)
+                .padding(.trailing, 40)
+            }
+        }
+        .background(Color("view-background").edgesIgnoringSafeArea(.all))
     }
 }
