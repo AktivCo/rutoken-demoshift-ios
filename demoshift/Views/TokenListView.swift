44ecbc64f8f2a0a3177fe2924296a0d7965361b2
Pavel Kamenov <kamenov@android-f6678de909ef1ac6.localdomain>
2023-03-15 12:16:04 +0300

    Fix intervals between cards

--- demoshift/Views/TokenListView.swift
+++ demoshift/Views/TokenListView.swift
@@ -22,52 +22,51 @@ struct TokenListView: View {
             NavigationLink(destination: VcrListView(),
                            isActive: self.$routingState.showStackedAddVCRView) {
                 EmptyView()
-            }
-            .isDetailLink(false)
+            }.isDetailLink(false)
             Text("Выберите Рутокен")
                 .font(.headline)
                 .padding(.top, 22)
-                .padding(.leading, 20)
             ScrollView(showsIndicators: false) {
-                if !state.readers.contains(where: { $0.type == .nfc || $0.type == .vcr }) {
-                    Button {
-                        self.routingState.showStackedAddVCRView.toggle()
-                    } label: {
-                        Text("Для подключения Рутокена с NFC используйте виртуальный считыватель")
-                            .font(.system(size: 16))
-                            .foregroundColor(Color("text-blue"))
-                            .padding(.top, 98)
-                            .padding(.bottom, 96)
-                    }
-                } else {
-                    VStack(spacing: 0) {
-                        HStack(alignment: .top, spacing: 0) {
-                            Text("Подключить Рутокен с NFC")
-                                .font(.headline)
-                                .padding(.top, 4)
-                            Spacer()
-                            Image("nfc-icon-gray")
-                                .renderingMode(.original)
-                                .resizable()
-                                .frame(width: 138, height: 138, alignment: .center)
+                VStack(alignment: .leading, spacing: 0) {
+                    if !state.readers.contains(where: { $0.type == .nfc || $0.type == .vcr }) {
+                        Button {
+                            self.routingState.showStackedAddVCRView.toggle()
+                        } label: {
+                            Text("Для подключения Рутокена с NFC используйте виртуальный считыватель")
+                                .font(.system(size: 16))
+                                .foregroundColor(Color("text-blue"))
+                                .padding(.top, 98)
+                                .padding(.bottom, 96)
                         }
-                        .padding(.vertical, 34)
-                        .padding(.leading, 23)
-                        .padding(.trailing, 20)
-                        Spacer()
+                    } else {
+                        VStack(spacing: 0) {
+                            HStack(alignment: .top, spacing: 0) {
+                                Text("Подключить Рутокен с NFC")
+                                    .font(.headline)
+                                    .padding(.top, 4)
+                                Spacer()
+                                Image("nfc-icon-gray")
+                                    .renderingMode(.original)
+                                    .resizable()
+                                    .frame(width: 138, height: 138, alignment: .center)
+                            }
+                            .padding(.vertical, 34)
+                            .padding(.leading, 23)
+                            .padding(.trailing, 20)
+                            Spacer()
+                        }
+                        .frame(maxHeight: 213)
+                        .background(Color("listitem-background"))
+                        .cornerRadius(30)
+                        .onTapGesture {
+                            interactorsContainer.tokenListInteractor?.didSelectToken(type: .NFC)
+                        }
                     }
-                    .frame(maxHeight: 213)
-                    .background(Color("listitem-background"))
-                    .cornerRadius(30)
-                    .onTapGesture {
-                        interactorsContainer.tokenListInteractor?.didSelectToken(type: .NFC)
-                    }
+                    usbTokenList()
+                    Spacer()
                 }
-                usbTokenList()
-                Spacer()
             }
             .padding(.top, 22)
-            .padding(.horizontal, 20)
             .background(EmptyView().sheet(isPresented: self.$state.showPinInputView, onDismiss: {
                 self.state.showPinInputView = false
                 self.state.taskStatus.errorMessage = ""
@@ -87,6 +86,7 @@ struct TokenListView: View {
                 CertListView()
             }))
         }
+        .padding(.horizontal, 20)
         .background(Color("view-background").edgesIgnoringSafeArea(.all))
     }
 
@@ -94,39 +94,41 @@ struct TokenListView: View {
     func usbTokenList() -> some View {
         Text("ПОДКЛЮЧЕННЫЕ РУТОКЕНЫ")
             .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.top, 24)
-        if #unavailable(iOS 16.0) {
-            Text("Для работы с устройствами Рутокен ЭЦП 2.0 и 3.0 требуется iOS 16 и новее")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 16))
-                .padding(.horizontal, 20)
-                .padding(.top, 66)
-        } else {
-            getUsbTokenItems()
-        }
-    }
-
-    @ViewBuilder
-    func getUsbTokenItems() -> some View {
-        if state.tokens.isEmpty {
-            VStack(alignment: .center, spacing: 0) {
+            .padding(.top, 32)
+        VStack(alignment: .center, spacing: 0) {
+            if #unavailable(iOS 16.0) {
+                Text("Для работы с устройствами Рутокен ЭЦП 2.0 и 3.0 требуется iOS 16 и новее")
+                    .multilineTextAlignment(.center)
+                    .font(.system(size: 16))
+                    .padding(.horizontal, 20)
+                    .padding(.top, 66)
+            } else if state.tokens.isEmpty {
                 Text("Нет доступных устройств. Подключите Рутокен контактно и он отобразится в этом разделе")
                     .multilineTextAlignment(.center)
                     .font(.system(size: 16))
                     .padding(.horizontal, 20)
                     .padding(.top, 66)
+            } else {
+                getUsbTokenItems()
+                    .padding(.top, 20)
             }
         }
-        ForEach(state.tokens) { token in
-            TokenCard(modelName: token.modelName,
-                      serial: token.serial,
-                      currentInterface: token.currentInterface,
-                      interfaces: token.supportedInterfaces)
+    }
+
+    @ViewBuilder
+    func getUsbTokenItems() -> some View {
+        VStack(spacing: 0) {
+            ForEach(state.tokens) { token in
+                TokenCard(modelName: token.modelName,
+                          serial: token.serial,
+                          currentInterface: token.currentInterface,
+                          interfaces: token.supportedInterfaces)
                 .allowsHitTesting(token.modelName != .unsupported)
                 .onTapGesture {
                     interactorsContainer.tokenListInteractor?.didSelectToken(token.serial, type: token.currentInterface)
                 }
+                .padding(.bottom, 10)
+            }
         }
-        .padding(.vertical, 10)
     }
 }
